3bacb09733f02d98e34f1f0f597b3183346d167b
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-11-11 10:33:14 -0700

    finished pw hashing

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt= require('bcryptjs');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -10,4 +11,13 @@ router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
 
+router.post('/hash', (req, res)=>{
+  // read a password from the body
+  // hash the password
+  // return it to the user in an object that looks like 
+  // {password:'original password', hash:'hashed password'}
+  const password = req.body.password;
+  const hash = bcrypt.hashSync(password, 8);
+  res.status(200).json({password, hash});
+})
 module.exports = router;
